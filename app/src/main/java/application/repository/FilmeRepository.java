7ae4b9d8c6275a378e4f11c1595a547a1d6276df
lucastamura <lucas10.tamura@gmail.com>
2024-09-09 17:52:36 +0000

    Criando reposirório dos filmes

---
+++ app/src/main/java/application/repository/FilmeRepository.java
@@ -0,0 +1,10 @@
+package application.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import application.model.Filme;
+
+public class FilmeRepository extends CrudRepository<Filme, Long>{
+    
+}
+
